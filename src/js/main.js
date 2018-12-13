2b2ff551a49f7ac5448c38eea1d7995e6a1df10d
20dima02 <20deshik02@gmail.com>
2018-12-13 20:20:05 +0200

    functionality for nav have been added

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
     $('.nav_link').click(function(element){
+        var scrollTo =$(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(scrollTo).offset().top
+        },1500)
         if (!$(element.target).hasClass('nav_link__active')){
             $('.nav_item').each(function(index, element){
                 $(element).find('.nav_link__active').removeClass('nav_link__active')
